b57d8f9dbb2a1ca2eb7968ff067d30e2084f855d
Y45m1n <yasmindeoliveira286@gmail.com>
2024-11-18 16:34:08 -0300

    edit

--- buson-mobile/buson-mobile/app/(tabs)/index.tsx
+++ buson-mobile/buson-mobile/app/(tabs)/index.tsx
@@ -1,35 +1,35 @@
-// import * as React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// import LoginScreen from './login_register/login';
-// import RegisterScreen from './login_register/register';
+import LoginScreen from './login_register/login';
+import RegisterScreen from './login_register/register';
 
-// import { Routes } from '../routes'; // Importando o sistema de navegação com o BottomTabNavigator
+import { Routes } from '../routes'; // Importando o sistema de navegação com o BottomTabNavigator
 
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
-// export default function App() {
+export default function App() {
 
-//   return (
-//     <NavigationContainer independent={true}>
-//       <Stack.Navigator initialRouteName="Login"
-//         screenOptions={{
-//           headerShown: false,  // Removendo o cabeçalho
-//         }}>
-//         <Stack.Screen name="Login" component={LoginScreen} options={{
-//           animation: 'fade'
-//         }} />
-//         <Stack.Screen name="Register" component={RegisterScreen} options={{
-//           animation: 'fade'
-//         }} />
-//         <Stack.Screen
-//           name="Home"
-//           component={Routes} // Aqui você usa o Routes com o BottomTabNavigator
-//           options={{ headerShown: false, animation: 'fade' }} // Esconde o header se necessário
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer >
-//   )
-// }
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,  // Removendo o cabeçalho
+        }}>
+        <Stack.Screen name="Login" component={LoginScreen} options={{
+          animation: 'fade'
+        }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{
+          animation: 'fade'
+        }} />
+        <Stack.Screen
+          name="Home"
+          component={Routes} // Aqui você usa o Routes com o BottomTabNavigator
+          options={{ headerShown: false, animation: 'fade' }} // Esconde o header se necessário
+        />
+      </Stack.Navigator>
+    </NavigationContainer >
+  )
+}
 
